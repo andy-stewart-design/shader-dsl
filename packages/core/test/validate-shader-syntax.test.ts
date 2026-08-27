ae5c8247f9d71979302489d69259de1032e317fb
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-08-27 08:34:16 -0400

    testing nested parentheses

--- packages/core/test/validate-shader-syntax.test.ts
+++ packages/core/test/validate-shader-syntax.test.ts
@@ -125,8 +125,8 @@ describe("shader syntax validation", () => {
       import { createFragmentShader, vec4 } from "shdr";
 
       export default createFragmentShader(({ coord, uniforms }) => {
-        const uv = (coord.xy / uniforms.resolution);
-        return vec4((uv.x), uv.y, 0, 1);
+        const uv = ((coord.xy / (uniforms.resolution)));
+        return vec4(((uv.x)), uv.y, 0, 1);
       });
     `;
 
